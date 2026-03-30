0000d2df397e9327515c50a61afd35a2761a8bc4
731096 <731096@student.dusd.net>
2026-03-30 15:25:08 -0700

    sound
    explosion

--- P7LockwoodUnit3/Assets/Scripts/PlayerController.cs
+++ P7LockwoodUnit3/Assets/Scripts/PlayerController.cs
@@ -7,21 +7,23 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver;
+    private Animator playerAnim;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround )
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver )
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
-        
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -33,6 +35,9 @@ public class PlayerController : MonoBehaviour
         } else if (collision.gameObject.CompareTag("Obstacle")) 
         {
             Debug.Log("Game Over");
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 2);
         }
     }
 }
