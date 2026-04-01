82384e40448d34e0e6d727476091d4fbc79c9de7
731096 <731096@student.dusd.net>
2026-04-01 15:25:51 -0700

    sound
    bonus

--- P7LockwoodUnit3/Assets/Scripts/PlayerController.cs
+++ P7LockwoodUnit3/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private AudioSource playerAudio;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    public ParticleSystem dirtParticle;
+    public ParticleSystem explosionParticle;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -12,6 +17,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAudio = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
         playerAnim = GetComponent<Animator>();
     }
@@ -24,6 +30,7 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -32,12 +39,16 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground")) 
         {
             isOnGround = true;
+            dirtParticle.Play();
         } else if (collision.gameObject.CompareTag("Obstacle")) 
         {
             Debug.Log("Game Over");
             gameOver = true;
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 2);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
--- P7LockwoodUnit3/Assets/Scripts/Spawnmanager.cs
+++ P7LockwoodUnit3/Assets/Scripts/Spawnmanager.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class Spawnmanager : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
+    public GameObject[] obstaclePrefabs;
     private Vector3 spawnPos = new Vector3(25, 1, 0);
     private float startDely = 2;
     private float repeatRate = 2;
-
+    private int randomObstacle;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,6 +21,7 @@ public class Spawnmanager : MonoBehaviour
     }
     void SpawnObstacle() 
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        randomObstacle = Random.Range(0, obstaclePrefabs.Length);
+        Instantiate(obstaclePrefabs[randomObstacle], spawnPos, obstaclePrefabs[randomObstacle].transform.rotation);
     }
 }
